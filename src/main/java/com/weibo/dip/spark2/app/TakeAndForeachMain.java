e3eabfcec6fe5eaeb306ccea7b399bf061da70b1
yurun <miao.work2010@gmail.com>
2016-09-25 16:21:59 +0800

    Add CollectAndForeachMain、TakeAndForeachMain

---
+++ src/main/java/com/weibo/dip/spark2/app/TakeAndForeachMain.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.weibo.dip.spark2.app;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * @author yurun
+ *
+ */
+public class TakeAndForeachMain {
+
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf();
+
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+
+		JavaRDD<Integer> distData = sc.parallelize(data);
+
+		distData.take(3).forEach(new Consumer<Integer>() {
+
+			@Override
+			public void accept(Integer t) {
+				System.out.println(t);
+			}
+
+		});
+
+		sc.close();
+	}
+
+}
